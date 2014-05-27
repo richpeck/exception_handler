507dc9f31e6bb404ee61fd54876e3c296029799b
richpeck <rpeck@frontlineutilities.co.uk>
2014-05-27 19:58:46 +0100

    Added Fixes

--- custom_error_pages.gemspec
+++ custom_error_pages.gemspec
@@ -19,7 +19,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.6"
-  spec.add_development_dependency "rake"
-  spec.add_development_dependency "haml"
-  spec.add_development_dependency "sass"
+  spec.add_development_dependency "rails", "~> 4.0.0"
+  spec.add_development_dependency "haml", "~> 4.0.5"
+  spec.add_development_dependency "sass", "~> 3.3.7"
 end
--- lib/custom_error_pages.rb
+++ lib/custom_error_pages.rb
@@ -1,14 +1,20 @@
 ###########################################
 
 require "action_dispatch"
+
 require "custom_error_pages/version"
+require "custom_error_pages/parser"
 
 ###########################################
 
 module CustomErrorPages
+
+	#Exception Handler
 	class Exceptions < Rails::Engine
 		initializer "custom_error_pages.configure_rails_initialization" do |app|
-			app.config.exceptions_app = ->(env) { CustomErrorPages::ExceptionController.action(:show).call(env) } #Error Pages
+			app.config.middleware.use "CustomErrorPages::Message" #Parser
+			app.config.exceptions_app = ->(env) { CustomErrorPages::ExceptionController.action(:show).call(env) } #Pages
 		end
 	end
+
 end
