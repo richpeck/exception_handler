0af4d61ac9888b070d14ed30d2e54d45054ca2ac
richpeck <rpeck@frontlineutilities.co.uk>
2014-05-29 10:58:50 +0100

    Added

---
+++ spec/exception_spec.rb
@@ -0,0 +1,16 @@
+###########################################
+
+require 'spec_helper'
+
+###########################################
+
+describe ExceptionHandler::Parser do
+
+  describe "404 Error" do
+    it "will show custom 404 error page" do
+    end
+  end
+
+end
+
+###########################################
