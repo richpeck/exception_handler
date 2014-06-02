18b245f11bf5be8387bd5f8aa35cb484b5155147
richpeck <rpeck@frontlineutilities.co.uk>
2014-06-02 15:56:21 +0100

    Added Fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,9 +10,8 @@ require 'coveralls'
 Coveralls.wear!
 
 #Rails
-ENV["RAILS_ENV"] = "test"
-require File.expand_path('../../../../config/environment', __FILE__)
-require 'rails/test_help'
+require File.expand_path("../dummy/config/environment", __FILE__)
+require 'rspec/rails'
 
 ###########################################
 
