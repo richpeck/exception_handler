033ed215c03bb6422835bd8ccb36e499b4a2ee46
richpeck <rpeck@frontlineutilities.co.uk>
2014-06-02 16:05:14 +0100

    Environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ require 'coveralls'
 Coveralls.wear!
 
 #Rails
-require File.expand_path("../rails/config/environment", __FILE__)
+require File.expand_path("../rails/config/environment.rb", __FILE__)
 require 'rspec/rails'
 
 ###########################################
