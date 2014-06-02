3a8e7edb395905c482ec1b992ce038498c2eece8
richpeck <rpeck@frontlineutilities.co.uk>
2014-06-02 13:59:45 +0100

    Added Fixes

--- spec/installation_spec.rb
+++ spec/installation_spec.rb
@@ -1,18 +1,19 @@
 ###########################################
 
 require 'spec_helper'
-require "generators/exception_handler/install_generator"
 
 ###########################################
 
 class InstallerSpec < Rails::Generators::TestCase
+
+	#Setup
 	tests ExceptionHandler::Generators::InstallGenerator
 	destination File.expand_path("../../tmp", __FILE__)
 
 	#Config Installer
-	test "should add config initializer" do 
-		run_generator %w(install)
-		File.exist?("config/intializers/exception_handler.rb").should be_true
+	test "Assert Config Files Are Created" do 
+		run_generator
+    assert_file "config/initializers/exception_handler.rb"
 	end
 
 end
