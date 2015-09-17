c7c335815ba677fb90a67a4835404d2d4bb95129
Richard Peck <rpeck@frontlineutilities.co.uk>
2015-09-17 09:21:07 +0100

    Readme

--- lib/generators/templates/config.rb
+++ lib/generators/templates/config.rb
@@ -1,15 +1,28 @@
-# Exception Handler
-###
-# You can add different settings using this block
-# Use the docs at http://github.com/richpeck/exception_handler for info
-###
+#################################################################################################
+##                                                                                             ##
+##      ______                     __  _                __  __                ____             ##
+##     / ____/  __________  ____  / /_(_)___  ____     / / / /___ _____  ____/ / /__  _____    ##
+##    / __/ | |/_/ ___/ _ \/ __ \/ __/ / __ \/ __ \   / /_/ / __ `/ __ \/ __  / / _ \/ ___/    ##
+##   / /____>  </ /__/  __/ /_/ / /_/ / /_/ / / / /  / __  / /_/ / / / / /_/ / /  __/ /        ##
+##  /_____/_/|_|\___/\___/ .___/\__/_/\____/_/ /_/  /_/ /_/\__,_/_/ /_/\__,_/_/\___/_/         ##
+##                      /_/                                                                    ##
+##                                                                                             ##
+#################################################################################################
+# You can add different settings using this file                                                #
+# Use the docs at http://github.com/richpeck/exception_handler for info                         #
+#################################################################################################
+
+# config/initializers/exception_handler.rb
 ExceptionHandler.setup do |config|
+
   # DB -
-  # Options = false / true
+  # Options = false / true / :table_name (must exist)
   config.db = false
+
   # Email -
   # Default = false / true
-  #  config.email =
+  # config.email =
+
   # Social
   config.social = {
     twitter: 'http://twitter.com/frontlineutils',
@@ -18,4 +31,5 @@ ExceptionHandler.setup do |config|
     youtube: 'https://youtube.com/user/frontlineutils',
     fusion: 'http://frontlinefusion.com/frontlineutils'
   }
+
 end
