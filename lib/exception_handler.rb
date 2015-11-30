8573651d3db8fd2e4f3509fc0b174a3652e7acce
Richard S Peck <rpeck@frontlineutilities.co.uk>
2015-11-30 15:42:27 +0000

    Config
    Former-commit-id: 5753371af54072444288bac6e8ee7904dd8d081d

--- lib/exception_handler.rb
+++ lib/exception_handler.rb
@@ -16,7 +16,7 @@ module ExceptionHandler
 	#Config
 	#https://github.com/thoughtbot/paperclip/blob/523bd46c768226893f23889079a7aa9c73b57d68/lib/paperclip/railtie.rb#L13
 	mattr_accessor :config
-	@@config = ExceptionHandler::Config.defaults.deep_merge({}) #-> instance of defaults invoked with @@config, edited with deep_merge
+	@@config = ExceptionHandler::Config.defaults.deep_merge!({}) #-> instance of defaults invoked with @@config, merged with deep_merge
 
 	#Exception Handler
 	class Exceptions < Rails::Engine
@@ -31,6 +31,9 @@ module ExceptionHandler
 
 		#Hook
 		initializer "exception_handler.configure_rails_initialization" do |app|
+			
+			ExceptionHandler.config.deep_merge! app.config.exception_handler if app.config.respond_to?(:exception_handler)
+
 			app.config.middleware.use "ExceptionHandler::Message" unless ExceptionHandler.config[:db] == false #Parser
 			app.config.exceptions_app = ->(env) { ExceptionHandler::ExceptionController.action(:show).call(env) } #Pages
 		end
