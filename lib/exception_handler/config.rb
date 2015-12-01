4920e73bc9a2a33ca4a8d551418fa2ec070b51bf
Richard S Peck <rpeck@frontlineutilities.co.uk>
2015-12-01 17:28:24 +0000

    URLs
    Former-commit-id: ebee1ded5f56feca30fd6163bd5fe26adfae0169

--- lib/exception_handler/config.rb
+++ lib/exception_handler/config.rb
@@ -7,19 +7,6 @@
 ###########################################
 
 module ExceptionHandler
-
-	# Accessible throughout the module
-	# http://stackoverflow.com/a/7853148/1143732
-	def self.social
-		@@social ||= {
-		    :twitter 	=> 	'http://twitter.com',
-		    :facebook 	=> 	'https://facebook.com',
-		    :linkedin 	=> 	'https://linkedin.com/company',
-		    :youtube 	=>	'https://youtube.com/user',
-		    :fusion 	=> 	'https://frontlinefusion.com',
-		}
-	end
-
 	class Config
   		mattr_accessor :db, :email, :social, :layouts
 
@@ -30,11 +17,18 @@ module ExceptionHandler
 				db:   	false, #-> defaults to :errors if true, else use :table_name
 				email: 	false, #-> need to integrate
 				social: {
-				    :twitter 	=> 	['frontlineutils', 		ExceptionHandler.social[:twitter]],
-				    :facebook 	=> 	['frontline.utilities', ExceptionHandler.social[:facebook]],
-				    :linkedin 	=> 	['frontline-utilities', ExceptionHandler.social[:linkedin]],
-				    :youtube 	=>	['frontlineutils', 		ExceptionHandler.social[:youtube]],
-				    :fusion 	=> 	['frontlineutils', 		ExceptionHandler.social[:fusion]],
+				    :facebook 	=> 	'frontline.utilities',
+				    :twitter 	=> 	'frontlineutils',
+				    :youtube 	=>	'frontlineutils',
+				    :linkedin 	=> 	'frontline-utilities',
+				    :fusion 	=> 	'frontlineutils',
+				    :url => {
+					    :facebook 	=> 	'https://facebook.com',
+					    :twitter 	=> 	'http://twitter.com',
+					    :youtube 	=>	'https://youtube.com/user',
+					    :linkedin 	=> 	'https://linkedin.com/company',
+					    :fusion 	=> 	'https://frontlinefusion.com',				    	
+				    },
 				},
 				layouts: {
 				    '400' => nil,
