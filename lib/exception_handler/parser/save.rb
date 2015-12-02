dca4bd0754fcd9b17e929968568cee19afc159e6
Richard S Peck <rpeck@frontlineutilities.co.uk>
2015-12-02 16:24:44 +0000

    Save Mechanism

--- lib/exception_handler/parser/save.rb
+++ lib/exception_handler/parser/save.rb
@@ -4,51 +4,46 @@ module ExceptionHandler
 
 			#Init
 			def initialize(exception, request, controller)
-				@exception, @request, @controller = exception, request, controller
+				@exception, @request, @controller, @user = exception, request, controller, user
 			end
 
 			#Save
 			def save
+				#http://blog.habanerohq.com/post/16800611137/selectively-silence-activerecord-logging
+				message = ""
 				ActiveRecord::Base.logger.silence do
-					ExceptionHandler::Error.create(relevant_info)
+					ExceptionHandler::Error.create info do |error|
+						message += "#{error.class_name}:\n "
+						#message += Rails.backtrace_cleaner.clean(error.trace.split("\n")).join("\n")
+					end
 				end
-	 			log(relevant_info)
+				Rails.logger.fatal message unless message.blank?
+				Rails.logger.info info[:usable_type]
+			end
+
+			private
+
+			#User
+			def user
+				@controller.send(:current_user) if @controller.respond_to?(:current_user)
 			end
 
 			#Info
-			def relevant_info(info = {})
-				info[:class_name] 	= @exception.class.to_s
-				info[:message] 		= @exception.to_s
-				info[:trace] 		= @exception.backtrace.join("\n")
-				info[:target_url] 	= @request.url
-				info[:referer_url] 	= @request.referer
-				info[:params] 		= @request.params.inspect
-				info[:user_agent] 	= @request.user_agent
-				if user
-					info[:usable_type] 	= user[:type]
-					info[:usable_id] 	= user[:id]
+			def info
+				info = {
+					class_name: 	@exception.class.to_s,
+					message: 		@exception.to_s,
+					trace: 			@exception.backtrace.join("\n"),
+					target_url: 	@request.url,
+					referer_url: 	@request.referer,
+					params: 		@request.params.inspect,
+					user_agent: 	@request.user_agent
+				}
+				if @user && @user.respond_to(:id)
+					info[:usable_type] 	= @user.model_name.human
+					info[:usable_id] 	= @user.id
 				end
 				return info
-			end
-
-			#User
-			def user(data = {})
-				# => refer to Joe's if want to find admin / user
-				if @controller.respond_to?(:current_user)
-					user = @controller.send :current_user
-					[:id].each do |field|
-						data[:id] = user.send(field) 	if user.respond_to?(field)
-						data[:type] = "User" 			if @controller.respond_to?("current_user")
-					end
-				end
-				return data
-			end
-
-			#Log
-			def log(info)
-				message = "#{info[:class_name]} (#{info[:message]}):\n "
-				message += Rails.backtrace_cleaner.clean(info[:trace].split("\n")).join("\n")
-				Rails.logger.fatal(message)
 			end
 
 		end
