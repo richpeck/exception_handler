9cb136c3a39c170efcca2398f47eb772f0b9249b
Richard S Peck <rpeck@frontlineutilities.co.uk>
2015-12-03 14:12:48 +0000

    Added Fixes
    Former-commit-id: 0b5c005b27086978f869bbd5d6ad6599bb299a7d

--- lib/exception_handler/parser/ignore.rb
+++ lib/exception_handler/parser/ignore.rb
@@ -2,18 +2,29 @@ module ExceptionHandler
 	module Parser
 		class Ignore
 
-			#Options
-			mattr_accessor :errors, :bots
-			#@@errors 	= [ActionController::RoutingError, AbstractController::ActionNotFound, ActiveRecord::RecordNotFound]
-			@@bots 		= Regexp.new %w(Baidu Gigabot Googlebot libwww-per lwp-trivial msnbot SiteUptime Slurp Wordpress ZIBB ZyBorg Yandex Jyxobot Huaweisymantecspider ApptusBot).join("|"), Regexp::IGNORECASE
-
+			#Init
 			def initialize(exception, request)
 				@exception, @request = exception, request
 			end
 
+			#Matches?
 			def match?
-				#return true if @@errors.include?(@exception.class) && @request.referer.blank?
+				return true if errors.include?(@exception.class) && @request.referer.blank?
 				return true if @request.user_agent =~ bots
+			end
+
+			private
+
+			#Has to be instance vars for env to handle ActionController::RoutingError constant etc
+
+			#Errors
+			def errors #-> http://ruby-doc.org/core-2.2.0/Array.html#class-Array-label-Creating+Arrays
+				Array(ActionController::RoutingError, AbstractController::ActionNotFound, ActiveRecord::RecordNotFound)
+			end
+
+			#Bots
+			def bots
+				Regexp.new %w(Baidu Gigabot Googlebot libwww-per lwp-trivial msnbot SiteUptime Slurp Wordpress ZIBB ZyBorg Yandex Jyxobot Huaweisymantecspider ApptusBot).join("|"), Regexp::IGNORECASE
 			end
 
 		end
