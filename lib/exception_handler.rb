80e58af676448e1974e92c7e5b7c8f275b303070
Richard S Peck <rpeck@frontlineutilities.co.uk>
2016-03-01 21:30:43 +0000

    Config

--- lib/exception_handler.rb
+++ lib/exception_handler.rb
@@ -14,15 +14,24 @@ Dir.glob(File.join(File.dirname(__FILE__), "exception_handler", '**/*.rb'), &met
 module ExceptionHandler
 
 	#Config
-	#Invoke instance of config (x.config) -- loads defaults
+	#Invoke instance of config (ExceptionHandler.config) -- loads defaults
 	mattr_accessor :config
+  @@config = nil
 
-	# Don't have prefix method return anything.
-    # This will keep Rails Engine from generating all table prefixes with the engines name
-    # http://stackoverflow.com/questions/19435214/rails-mountable-engine-with-isolate-namespace-but-without-prefixed-namespace-on
-    def self.table_name_prefix
-    	#No prefix
+	# Class methods
+    class << self
+    	# Don't have prefix method return anything.
+    	# This will keep Rails Engine from generating all table prefixes with the engines name
+    	# http://stackoverflow.com/questions/19435214/rails-mountable-engine-with-isolate-namespace-but-without-prefixed-namespace-on
+	    def table_name_prefix
+	    	#No prefix
+	    end
+
+	    #def config
+		#	ExceptionHandler::Config.new nil
+	    #end
     end
+
 
 	#Exception Handler
 	class Engine < Rails::Engine
@@ -33,16 +42,29 @@ module ExceptionHandler
 		isolate_namespace ExceptionHandler
 
 		#Assets
-		%w(error.css close.png alert.png home.png connect/facebook.png connect/fusion.png connect/linkedin.png connect/youtube.png connect/twitter.png).each { |a| config.assets.precompile << "exception_handler/#{a.to_s}" }
+		config.assets.precompile << %w(exception_handler/**)
 
 		#Hook
 		initializer "exception_handler.configure_rails_initialization" do |app|
-			
-			#Options
-			ExceptionHandler.config = ExceptionHandler::Config.new(app.config.respond_to?(:exception_handler) ? app.config.exception_handler : nil)
 
-			#Middleware
-			app.config.middleware.use ExceptionHandler::Parse if ExceptionHandler.config.db 						# Saves to DB
+      #Has to be loaded after Rails app
+      #Boot order:
+      #1)  require "config/boot.rb" to setup load paths
+      #2)  require railties and engines
+      #3)  Define Rails.application as "class MyApp::Application < Rails::Application"
+      #4)  Run config.before_configuration callbacks
+      #5)  Load config/environments/ENV.rb
+      #6)  Run config.before_initialize callbacks
+      #7)  Run Railtie#initializer defined by railties, engines and application.
+      #    One by one, each engine sets up its load paths, routes and runs its config/initializers/* files.
+      #8)  Custom Railtie#initializers added by railties, engines and applications are executed
+      #9)  Build the middleware stack and run to_prepare callbacks
+      #10) Run config.before_eager_load and eager_load! if eager_load is true
+      #11) Run config.after_initialize callbacks
+      #http://api.rubyonrails.org/classes/Rails/Application.html#class-Rails::Application-label-Booting+process
+      ExceptionHandler.config = ExceptionHandler::Config.new app.config.try(:exception_handler)
+
+			app.config.middleware.use ExceptionHandler::Parse if ExceptionHandler.config.db 						            # Saves to DB
 			app.config.exceptions_app = ->(env) { ExceptionHandler::ExceptionController.action(:show).call(env) } 	# Routes to Controller	
 		end
 
