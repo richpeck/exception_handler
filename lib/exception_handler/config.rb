76d36a8a9dff34414d189f1443548d90b5134dcb
Richard S Peck <rpeck@frontlineutilities.co.uk>
2016-03-09 07:59:04 +0000

    Defaults
    Former-commit-id: e06614f8d28a74bbc854df727d8fec262b8a1bf0
    Former-commit-id: 91edc85a4e8c886a1b3d06f8f1182ec0ad39b5e4

--- lib/exception_handler/config.rb
+++ lib/exception_handler/config.rb
@@ -59,6 +59,7 @@ module ExceptionHandler
           '404' => nil, #-> 404 Callback (needs improving big time) Use the following: '404' => <<-EOF redirect_to root_url, notice: "Hello" EOF
           '500' => 'exception'
         },
+        '404'   => nil #-> gives direction to 404 errors (default loads "show" view of ExceptionsController)
       }
 
     ###########################################
