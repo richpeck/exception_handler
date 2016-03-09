a7822185baaed4a560f0a94315490c10054c6c64
Richard S Peck <rpeck@frontlineutilities.co.uk>
2016-03-09 09:47:30 +0000

    Views
    Former-commit-id: 3584f8cd90a5765915d13f81ac19f8f5c1e608b6
    Former-commit-id: 36147cc053abf4821c66a15940dc5358bbf6d892

--- lib/generators/exception_handler/views_generator.rb
+++ lib/generators/exception_handler/views_generator.rb
@@ -1,42 +1,42 @@
 module ExceptionHandler
-	class ViewsGenerator < Rails::Generators::Base
+  class ViewsGenerator < Rails::Generators::Base
 
-		#Views
-		@@views = %w(views controllers helpers models assets)
+    #Views
+    @@views = %w(views controllers helpers models assets)
 
-		#Options
-		class_option :files, aliases: "-v", default: @@views, type: :array, desc: "Select file types (views, models, controllers, assets)"
+    #Options
+    class_option :files, aliases: "-v", default: @@views, type: :array, desc: "Select file types (views, models, controllers, assets)"
 
-		#Needed to reference files
-		source_root File.expand_path("../../../../app", __FILE__) 
+    #Needed to reference files
+    source_root File.expand_path("../../../../app", __FILE__) 
 
-		###########################################
+    ###########################################
 
-		#Files
-		def create_files
-			generate_files options.files
-		end
+    #Files
+    def create_files
+      generate_files options.files
+    end
 
-		###########################################
+    ###########################################
 
-		protected
+    protected
 
-		#File Generator
-		def generate_files args
+    #File Generator
+    def generate_files args
 
-			#Valid?
-			return raise args.inspect unless args.nil? || (args-@@views).empty?
+      #Valid?
+      return raise args.inspect unless args.nil? || (args-@@views).empty?
 
-			#Types
-			for arg in args do
-				directory arg, "app/#{arg}"
-			end
+      #Types
+      for arg in args do
+        directory arg, "app/#{arg}"
+      end
 
-			#Success
-			puts "Files transferred successfully"
-		end
+      #Success
+      puts "Files transferred successfully"
+    end
 
-		###########################################
+    ###########################################
 
-	end
+  end
 end
