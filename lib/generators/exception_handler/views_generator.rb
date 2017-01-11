f5505110a8e7c96b76f60adc7b522178db0e8d94
Richard S Peck <rpeck@frontlineutilities.co.uk>
2017-01-11 17:12:02 +0000

    Readme

--- lib/generators/exception_handler/views_generator.rb
+++ lib/generators/exception_handler/views_generator.rb
@@ -5,14 +5,14 @@ module ExceptionHandler
     ###########################################
     ###########################################
 
-    #Views
-    VIEWS = %w(views controllers models assets)
+      #Views
+      VIEWS = %w(views controllers models assets)
 
-    #Options
-    class_option :files, aliases: "-v", default: VIEWS, type: :array, desc: "Select file types (views, models, controllers, assets)"
+      #Options
+      class_option :files, aliases: "-v", default: VIEWS, type: :array, desc: "Select file types (views, models, controllers, assets)"
 
-    #Needed to reference files
-    source_root File.expand_path("../../../../app", __FILE__)
+      #Needed to reference files
+      source_root File.expand_path("../../../../app", __FILE__)
 
     ###########################################
     ###########################################
