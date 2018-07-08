93e176db931dc70cc438cc6002efb316743ae83a
Richard Peck <rpeck@frontlineutilities.co.uk>
2018-07-08 12:10:28 +0100

    Version

--- spec/exception_handler_spec.rb
+++ spec/exception_handler_spec.rb
@@ -16,12 +16,6 @@ require 'spec_helper'
 ###############################################
 ###############################################
 
-class TestPlugin::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, TestPlugin
-  end
-end
-
 # => ExceptionHandler (base)
 RSpec.describe ExceptionHandler do
 
