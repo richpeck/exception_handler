e4c18d701d2d2a001d9e54bf419e10e9eaeff542
Richard Peck <rpeck@frontlineutilities.co.uk>
2018-07-08 11:40:43 +0100

    Dummy App Spec

--- spec/exception_handler_spec.rb
+++ spec/exception_handler_spec.rb
@@ -16,6 +16,12 @@ require 'spec_helper'
 ###############################################
 ###############################################
 
+class TestPlugin::Test < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, TestPlugin
+  end
+end
+
 # => ExceptionHandler (base)
 RSpec.describe ExceptionHandler do
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,31 +14,63 @@
 require 'bundler/setup'
 require 'rails'
 require 'exception_handler'
+require 'rails/test_help'
+
+###############################################
+###############################################
+
+require_relative './dummy/config/environment'
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("./dummy/db/migrate", __dir__)]
 
 ###############################################
 ###############################################
 
 # => ENV
 ENV["RAILS_ENV"] ||= "test"
-require File.expand_path("../../config/environment", __FILE__)
+
+# => Production Only
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
 # => ActiveRecord
 ActiveRecord::Migration.maintain_test_schema!
 
+# Load fixtures from the engine
+if ActiveSupport::TestCase.respond_to?(:fixture_path=)
+  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
+  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
+  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
+  ActiveSupport::TestCase.fixtures :all
+end
+
 ###############################################
 ###############################################
 
+# => http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  # rspec-expectations config goes here. You can use an alternate
+  # assertion/expectation library such as wrong or the stdlib/minitest
+  # assertions if you prefer.
+  config.expect_with :rspec do |expectations|
+
+    # This option will default to `true` in RSpec 4. It makes the `description`
+    # and `failure_message` of custom matchers include text for helper methods
+    # defined using `chain`, e.g.:
+    #     be_bigger_than(2).and_smaller_than(4).description
+    #     # => "be bigger than 2 and smaller than 4"
+    # ...rather than:
+    #     # => "be bigger than 2"
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+
+    # => Default Syntax
+    c.syntax = :expect
+  end
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
 
   # => Rails (Fixtures etc)
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
