511fe276866803ebc2e90cd53ea9f3a9409f93e9
Richard Peck <rpeck@frontlineutilities.co.uk>
2018-07-12 11:31:14 +0100

    Coveralls ENV Var

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,10 +46,10 @@ ActiveRecord::Migration.maintain_test_schema!
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-  ActiveSupport::TestCase.fixtures :all
+   ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
+   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
+   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
+   ActiveSupport::TestCase.fixtures :all
 end
 
 ###############################################
